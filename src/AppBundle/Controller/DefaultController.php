e7f4768e5ab1cab980b1f778c631eb71094e050f
Sebastian <juan.ry@globant.com>
2019-01-06 22:45:03 -0500

    Routes and templates added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,4 +16,20 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('home/index.html.twig');
     }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction(Request $request) {
+
+        return $this->render('home/about.html.twig');
+    }
+
+    /**
+     * @Route("/contact/{place}", name="contact")
+     */
+    public function blogAction(Request $request, $place = 'all') {
+
+        return $this->render('home/contact.html.twig', ['place' => $place]);
+    }
 }
